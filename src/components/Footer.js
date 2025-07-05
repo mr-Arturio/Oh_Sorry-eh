c3bd348b65da98f4737268dfa94ea7493c901f3e
mr-Arturio <mr.arturio@gmail.com>
2025-07-05 19:46:41 -0400

    add link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <div className="mt-8 text-center text-gray-500 text-sm">
       <p>Made with ❤️ for the most Canadian hackathon experience, eh! 🏒🍁</p>
+      <p className="mt-4">Build by <a href="https://github.com/mr-Arturio" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">Artur T.</a></p>
     </div>
   );
 }
